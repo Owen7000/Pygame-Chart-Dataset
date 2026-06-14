05c2b866914d2485eeff8f8a995e679c9544f485
Owen <77976026+Owen7000@users.noreply.github.com>
2026-06-14 22:32:32 +0100

    Add docstrings to class and type def

--- PyChartDataset/dataset.py
+++ PyChartDataset/dataset.py
@@ -5,6 +5,7 @@ from typing import Deque
 from collections import deque
 
 type Number = int | float
+"""A numeric type that accepts either integers or floating-point decimals"""
 
 class Dataset:
     def __init__(self, dataset_name:str, max_data_length:int=10) -> None:
@@ -15,13 +16,48 @@ class Dataset:
 
     @property
     def data(self) -> list[Number]:
+        """data Get the stored data as a list
+
+        Examples:
+            >>> from dataset import Dataset
+            >>> Dataset("Dataset name").data
+            []
+
+        Returns:
+            list[Number]: The data currently stored in the dataset
+        """
         return list(self._data)
 
     def append(self, value: Number) -> None:
+        """append Add 1 piece of data to the set
+
+        Append to the end of the dataset.
+        If the data is at the maximum length, the first item will be removed and all other shifted left by one.
+
+        Args:
+            value (Number): The Number to add to the dataset
+        """
         self._data.append(value)
 
     def clear(self) -> None:
+        """clear Empty the dataset
+
+        Removes all items from the internal deque
+        """
         self._data.clear()
 
     def __len__(self) -> int:
+        """__len__ Get the length of the stored data
+
+        Returns the length of the internal data deque
+
+        Examples:
+            >>> from dataset import Dataset
+            >>> ds1 =  Dataset("Dataset name")
+            >>> len(ds1)
+            0
+
+        Returns:
+            int: The length of the data deque
+        """
         return len(self._data)
