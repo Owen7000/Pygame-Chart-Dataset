c02966ad293623a607f9b3824b676db463599e0b
Owen <77976026+Owen7000@users.noreply.github.com>
2026-06-14 21:35:21 +0100

    Move setup.py to correct location

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyChartDataset',
+    version='0.1.0',
+    author='Owen Plimer',
+    author_email='owen@plimsoft.co.uk',
+    description='A helper class for use with Pygame-Chart charts',
+    packages=find_packages()
+)
